c0ffaf97b5c715d6740d07c05e76cbce264c2e08
Charlie Smith <charlie@signalfx.com>
2016-09-14 16:13:38 -0400

    add plugin_flush_callback_name to plugin.c

--- src/daemon/plugin.c
+++ src/daemon/plugin.c
@@ -1333,6 +1333,29 @@ static void plugin_flush_timeout_callback_free (void *data)
 	sfree(cb);
 } /* static void plugin_flush_callback_free */
 
+static char *plugin_flush_callback_name (const char *name)
+{
+	const char *flush_prefix = "flush/";
+	size_t prefix_size;
+	char *flush_name;
+	size_t name_size;
+
+	prefix_size = strlen(flush_prefix);
+	name_size = strlen(name);
+
+	flush_name = malloc (name_size + prefix_size + 1);
+	if (flush_name == NULL)
+	{
+		ERROR ("plugin_flush_callback_name: malloc failed.");
+		return (NULL);
+	}
+
+	sstrncpy (flush_name, flush_prefix, prefix_size + 1);
+	sstrncpy (flush_name + prefix_size, name, name_size + 1);
+
+	return flush_name;
+} /* static char *plugin_flush_callback_name */
+
 int plugin_register_flush (const char *name,
 		plugin_flush_cb callback, user_data_t *ud)
 {
